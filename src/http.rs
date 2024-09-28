97181db0dc57a2d7bfeb334f348f5a6f84824b7a
Ryan Fowler <ryan.fowler19@gmail.com>
2024-09-28 14:04:54 -0700

    Improve url parsing

--- src/http.rs
+++ src/http.rs
@@ -7,7 +7,6 @@ use reqwest::{
     Method, Url, Version,
 };
 use termcolor::{BufferedStandardStream, ColorChoice};
-use url::ParseError;
 
 use crate::{
     aws_sigv4,
@@ -17,20 +16,15 @@ use crate::{
     Cli, Http, APP_STRING,
 };
 
-static SCHEME_HTTP: &str = "http";
-static SCHEME_HTTPS: &str = "https";
-
 static DEFAULT_CONNECT_TIMEOUT_MS: u64 = 30_000;
-static DEFAULT_TIMEOUT_SECONDS: u64 = 600;
 
 pub(crate) fn make_request(opts: &Cli, verbosity: Verbosity) -> Result<Option<Response>, Error> {
-    let (mut url, no_scheme) = parse_url(&opts.url)?;
+    let url = parse_url(&opts.url)?;
     let method = parse_method(opts.method.as_deref())?;
     let headers = parse_headers(&opts.header)?;
 
     let mut builder = ClientBuilder::new()
         .use_rustls_tls()
-        .timeout(Duration::from_secs(DEFAULT_TIMEOUT_SECONDS))
         .connect_timeout(Duration::from_millis(DEFAULT_CONNECT_TIMEOUT_MS));
     if let Some(v) = opts.http {
         builder = match v {
@@ -65,36 +59,27 @@ pub(crate) fn make_request(opts: &Cli, verbosity: Verbosity) -> Result<Option<Re
         }
     }
 
-    match client.execute(req) {
-        Ok(res) => Ok(Some(res)),
-        Err(err) => {
-            if no_scheme && url.set_scheme(SCHEME_HTTP).is_ok() {
-                let headers = parse_headers(&opts.header)?;
-                let req = build_request(
-                    &client,
-                    method,
-                    url,
-                    headers,
-                    opts.aws_sigv4.as_deref(),
-                    opts.http.as_deref(),
-                )?;
-                client.execute(req).map(Some).map_err(|e| e.into())
-            } else {
-                Err(err.into())
-            }
-        }
-    }
+    client.execute(req).map(Some).map_err(Into::into)
 }
 
-fn parse_url(url: &str) -> Result<(Url, bool), ParseError> {
-    // TODO(ryanfowler): We can do this better.
-    if !url.contains("://") {
-        let url = Url::parse(&format!("{SCHEME_HTTPS}://{url}"))?;
-        Ok((url, true))
-    } else {
-        let url = Url::parse(url)?;
-        Ok((url, false))
-    }
+fn parse_url(url: &str) -> Result<Url, Error> {
+    Url::parse(url)
+        .or_else(|err| {
+            if matches!(err, url::ParseError::RelativeUrlWithoutBase) {
+                Url::parse(&["https://", url.strip_prefix("//").unwrap_or(url)].concat())
+            } else {
+                Err(err)
+            }
+        })
+        .map_err(Into::into)
+        .and_then(|url| {
+            if ["http", "https"].contains(&url.scheme()) {
+                Ok(url)
+            } else {
+                let msg = format!("url scheme '{}' not supported", url.scheme());
+                Err(Error::new(msg))
+            }
+        })
 }
 
 fn parse_method(input: Option<&str>) -> Result<Method, Error> {
@@ -165,4 +150,24 @@ fn sign_aws_sigv4(opts: &str, req: &mut Request) -> Result<(), Error> {
 
 fn get_sigv4_var(key: &str) -> Result<String, Error> {
     env::var(key).map_err(|_| Error::new(format!("aws-sigv4: {key} must be provided")))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::parse_url;
+
+    #[test]
+    fn test_parse_url() {
+        let url = parse_url("http://example.com").expect("no error");
+        assert_eq!(url.as_str(), "http://example.com/");
+
+        let url = parse_url("https://example.com").expect("no error");
+        assert_eq!(url.as_str(), "https://example.com/");
+
+        let url = parse_url("//example.com").expect("no error");
+        assert_eq!(url.as_str(), "https://example.com/");
+
+        let url = parse_url("example.com").expect("no error");
+        assert_eq!(url.as_str(), "https://example.com/");
+    }
 }
