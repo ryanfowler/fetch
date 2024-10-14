7734d47a7f9510910541837eb579da45bf49111e
Ryan Fowler <ryan.fowler19@gmail.com>
2024-10-14 14:31:59 -0700

    Automate embedding of treesitter grammars

--- build.rs
+++ build.rs
@@ -1,63 +1,53 @@
 use std::path::PathBuf;
 
-struct Grammar {
-    name: &'static str,
-    prefix: Option<&'static str>,
-    files: &'static [&'static str],
+use serde::Deserialize;
+
+static RAW_GRAMMARS: &str = include_str!("grammars/grammars.toml");
+
+#[derive(Deserialize)]
+struct Grammars {
+    language: Vec<Language>,
 }
 
-const GRAMMARS: &[Grammar] = &[
-    Grammar {
-        name: "html",
-        prefix: None,
-        files: &["parser.c", "scanner.c"],
-    },
-    Grammar {
-        name: "json",
-        prefix: None,
-        files: &["parser.c"],
-    },
-    Grammar {
-        name: "toml",
-        prefix: None,
-        files: &["parser.c", "scanner.c"],
-    },
-    Grammar {
-        name: "xml",
-        prefix: Some("xml"),
-        files: &["parser.c", "scanner.c"],
-    },
-    Grammar {
-        name: "yaml",
-        prefix: None,
-        files: &["parser.c", "scanner.c"],
-    },
-];
+#[derive(Deserialize)]
+struct Language {
+    name: String,
+    files: Vec<String>,
+    headers: Vec<String>,
+}
 
 fn main() {
-    for grammer in GRAMMARS {
-        let mut paths = vec!["grammars", grammer.name];
-        if let Some(prefix) = grammer.prefix {
-            paths.push(prefix);
-        }
-        paths.push("src");
-        let dir: PathBuf = paths.iter().collect();
+    let grammars: Grammars = toml::from_str(RAW_GRAMMARS).unwrap();
+    for language in grammars.language {
+        let dir: PathBuf = ["grammars", &language.name].iter().collect();
 
         let mut build = cc::Build::new();
-        for file in grammer.files {
-            build.file(dir.join(file));
+        let include = if language.name.as_str() == "xml" {
+            dir.join("xml").join("src")
+        } else {
+            dir.join("src")
+        };
+        build.include(include);
+
+        for file in language.files {
+            let path = dir.join(file);
+            build.file(&path);
+            println!("cargo:rerun-if-changed={}", path.to_str().unwrap());
         }
+        for header in language.headers {
+            let path = dir.join(header);
+            println!("cargo:rerun-if-changed={}", path.to_str().unwrap());
+        }
+
         build
             .include(&dir)
             .flag_if_supported("-Wno-implicit-fallthrough")
             .flag_if_supported("-Wno-sign-compare")
             .flag_if_supported("-Wno-trigraphs")
             .flag_if_supported("-Wno-unused-but-set-variable")
+            .flag_if_supported("-Wno-unused-function")
             .flag_if_supported("-Wno-unused-parameter")
             .flag_if_supported("-Wno-unused-value")
-            .compile(&format!("tree-sitter-{}", grammer.name));
-
-        let header_path = dir.join("tree_sitter").join("parser.h");
-        println!("cargo:rerun-if-changed={}", header_path.to_str().unwrap());
+            .compile(&format!("tree-sitter-{}", language.name));
     }
 }
