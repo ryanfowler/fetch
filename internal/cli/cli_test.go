a067df28b29fe08bb074d920af24314d0008eced
Ryan Fowler <ryan.fowler19@gmail.com>
2025-02-23 14:16:43 -0500

    Ensure line length of help output stays under 80 characters

---
+++ internal/cli/cli_test.go
@@ -0,0 +1,26 @@
+package cli
+
+import (
+	"strings"
+	"testing"
+	"unicode/utf8"
+
+	"github.com/ryanfowler/fetch/internal/printer"
+)
+
+func TestCLI(t *testing.T) {
+	app, err := Parse(nil)
+	if err != nil {
+		t.Fatalf("unable to parse cli: %s", err.Error())
+	}
+	p := printer.NewHandle(printer.ColorOff).Stdout()
+
+	// Verify that no line of the help command is over 80 characters.
+	app.PrintHelp(p)
+	for line := range strings.Lines(string(p.Bytes())) {
+		line = strings.TrimSuffix(line, "\n")
+		if utf8.RuneCountInString(line) > 80 {
+			t.Fatalf("line too long: %q", line)
+		}
+	}
+}
